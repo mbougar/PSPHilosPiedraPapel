19df484b5b174dbc87c35fa1cb5b314aacf2f363
mbougar <146754886+mbougar@users.noreply.github.com>
2024-12-13 03:25:26 +0100

    Version con jugadas fijas

--- PSPHilosPiedraPapel/Program.cs
+++ PSPHilosPiedraPapel/Program.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-
 namespace PSPHilosPiedraPapel
 {
     internal static class Program
     {
-        private static readonly object Locker = new object();
-        private static readonly string[] Opciones = { "Piedra", "Papel", "Tijera" };
+        private static readonly object Locker = new();
+        private static readonly string[] Opciones = ["Piedra", "Papel", "Tijera"];
         private static int _ronda = 1;
-        
+
         private static List<string> _jugadores =
         [
             "Jugador 1", "Jugador 2", "Jugador 3", "Jugador 4",
@@ -17,106 +13,106 @@ namespace PSPHilosPiedraPapel
             "Jugador 9", "Jugador 10", "Jugador 11", "Jugador 12",
             "Jugador 13", "Jugador 14", "Jugador 15", "Jugador 16"
         ];
-        
-        private static List<string>[] _resultados = new List<string>[16];
-        private static List<bool> _terminados = [];
+
+        private static Dictionary<string, List<string>> _resultados = new Dictionary<string, List<string>>();
+        private static Dictionary<string, bool> _terminados = new Dictionary<string, bool>();
 
         private static void Main()
         {
-            CrearJugadores();
-        }
-
-        private static void CrearJugadores()
-        {
-            Console.WriteLine("Jugadores: ");
-            
-            foreach (var nombre in _jugadores)
-            {
-                var hilo = new Thread(() => Jugar(nombre));
-                hilo.Start();
-            }
-            
+            Console.WriteLine("Iniciando torneo de Piedra, Papel o Tijera...");
             GestionarTorneo();
         }
 
         private static void GestionarTorneo()
         {
-            if (_ronda == 5)
+            foreach (var jugador in _jugadores)
             {
-                Console.WriteLine($"Ganador: {_jugadores}");
-                return;
+                _resultados[jugador] = [];
+                var hilo = new Thread(() => Jugar(jugador));
+                hilo.Start();
             }
             
-            while (_terminados.Count != _jugadores.Count)
+            while (_jugadores.Count > 1)
             {
+                while (_terminados.ContainsValue(false))
+                {
+                    Thread.Sleep(500);
+                }
                 
+                Console.WriteLine($"\n--- Ronda {_ronda} ---");
+                
+                var ganadores = new List<string>();
+
+                for (var i = 0; i < _jugadores.Count; i += 2)
+                {
+                    var ganadorIndex = CompararResultados(_resultados[_jugadores[i]], _resultados[_jugadores[i + 1]]) - 1;
+                    ganadores.Add(_jugadores[i + ganadorIndex]);
+                    Console.WriteLine($"Partida: {_jugadores[i]} vs {_jugadores[i + 1]} - Ganador: {_jugadores[i + ganadorIndex]}");
+                    _terminados[_jugadores[i]] = false;
+                    _terminados[_jugadores[i + 1]] = false;
+                }
+                
+                _jugadores = ganadores;
+                _ronda++;
             }
 
-            for (var i = 0; i < _terminados.Count; i += 2)
-            {
-                var resultadosJugador1 = _resultados[i];
-                var resultadosJugador2 = _resultados[i + 1];
-                
-                var ganador = CompararResultados(resultadosJugador1, resultadosJugador2);
-                _jugadores.RemoveAt(i + 2 - ganador);
-            }
-            
-            _resultados = new List<string>[_resultados.Length / 2];
-            _terminados = [];
-            _ronda++;
-            
-            GestionarTorneo();
+            Console.WriteLine($"\nGanador del torneo: {_jugadores[0]}\n");
+            _jugadores = [];
         }
         
-        static void Jugar(string nombre)
+        private static void Jugar(string name)
         {
-            if (!_jugadores.Contains(nombre) || _jugadores.Count == 1)
-            {
-                return;
-            }
-            
-            int index;
-            
             lock (Locker)
             {
-                index = _jugadores.IndexOf(nombre);
+                _terminados[name] = false;
+            }
+            
+            while (_jugadores.Contains(name))
+            {
                 var random = new Random();
-                _resultados[index] =
-                [
+                var jugada = new List<string>
+                {
                     Opciones[random.Next(Opciones.Length)],
                     Opciones[random.Next(Opciones.Length)],
                     Opciones[random.Next(Opciones.Length)]
-                ];
-                _terminados.Add(true);
+                };
+                
+                _resultados[name] = jugada;
+                _terminados[name] = true;
+                Console.WriteLine($"{name} jugó: {string.Join(", ", jugada)}");
+                
+                while (_terminados[name])
+                {
+                    Thread.Sleep(100);
+                }
             }
             
-            Jugar(nombre);
+            _terminados[name] = true;
         }
 
         private static int CompararResultados(List<string> resultadosJugador1, List<string> resultadosJugador2)
         {
-            var resultado = resultadosJugador1.Select((t, i) => (t, resultadosJugador2[i]) switch
-                {
-                    ("Piedra", "Tijera") => 1,
-                    ("Tijera", "Papel") => 1,
-                    ("Papel", "Piedra") => 1,
-                    ("Tijera", "Piedra") => -1,
-                    ("Papel", "Tijera") => -1,
-                    ("Piedra", "Papel") => -1,
-                    _ => 0
-                })
-                .Sum();
+            var puntosJugador1 = 0;
+            var puntosJugador2 = 0;
 
-            return resultado > 0 ? 1 : 2;
+            for (var i = 0; i < 3; i++)
+            {
+                switch ((resultadosJugador1[i], resultadosJugador2[i]))
+                {
+                    case ("Piedra", "Tijera"):
+                    case ("Tijera", "Papel"):
+                    case ("Papel", "Piedra"):
+                        puntosJugador1++;
+                        break;
+                    case ("Tijera", "Piedra"):
+                    case ("Papel", "Tijera"):
+                    case ("Piedra", "Papel"):
+                        puntosJugador2++;
+                        break;
+                }
+            }
+
+            return puntosJugador1 > puntosJugador2 ? 1 : 2;
         }
     }
 }
-
-    // Creamos una nueva lista de hilos del tamaño de _nombresHilos, los hacemos competir recorriendo la lista de dos a dos,
-    // eliminamos a los perdedores de _nombresHilos y llamamos recursivamente a la funcion (creando una nueva lista de hilos de mitad de tamaño)
-    // hasta que solo quede un nombre. Posible Problema: son hilos nuevos cada ietracion no un único hilo que sobrevive todas las iteraciones.
-            
-    // Para solucionarlo podemos usar funciones recursivas para que cada hilo vaya escribiendo en cola en una lista de resultados e ir eliminando a los hilos cunado no aparecen
-    // en la lista de ganadores. Para ello necesitariamos crear un diccionario con los nombres de cada hilo y sus resultados y una lista de nombres de la cual los
-    // hilos obtengan su nombre al principio del programa.
-            
